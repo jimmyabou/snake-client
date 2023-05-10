e0a43b59f79658e5a3f28be3324fcc13852b4d2c
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-10 17:33:38 -0400

    finalized

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
+const { IP, PORT } = require("./constants");
 const net = require("net");
 const connect = function () {
-  const conn = net.createConnection({host: '165.227.47.243', port: 50541});
+  const conn = net.createConnection({host: IP, port: PORT});
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -19,10 +20,3 @@ const connect = function () {
 };
 module.exports={connect};
 
-
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
-
-// setInterval(function, delay, param1, param2, ...)
---
+++ constants.js
@@ -0,0 +1,7 @@
+//host: '165.227.47.243', port: 50541
+const IP="165.227.47.243";
+const PORT=50541;
+module.exports = {
+  IP,
+  PORT,
+};
